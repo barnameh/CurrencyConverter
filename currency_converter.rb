15d37cc960156a9775c272d5daeadd661117c0b1
barnameh <farimahd@gmail.com>
2016-10-15 23:08:38 -0400

    Added convert method to take a Currency object and a requested currency code that is the same currency code as the Currency object's and return a Currency object equal to the one passed in.

--- currency_converter.rb
+++ currency_converter.rb
@@ -6,4 +6,9 @@ class CurrencyConverter
     @currency_code_hash = currency_code_hash
   end
 
+  def convert(a_currency, a_currency_code)
+    raise DifferentCurrencyCodeError if a_currency.currency_code != a_currency_code
+    Currency.new(a_currency.amount, a_currency_code)
+  end
+
 end
