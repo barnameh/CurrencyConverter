f7a0a281b4a170f99cce3b14e010cd8248a23984
barnameh <farimahd@gmail.com>
2016-10-16 19:49:37 -0400

    Tested CurrencyConverter and fixed a mistake in calculating conversion in the convert method

--- currency_converter.rb
+++ currency_converter.rb
@@ -1,26 +1,26 @@
-require_relative 'currency'
-require_relative 'unknown_currency_code_error'
+require_relative 'currency'                     # => true
+require_relative 'unknown_currency_code_error'  # => true
 
 class CurrencyConverter
 
   def initialize(currency_code_hash = {"USD" => 1.0, "EUR" => 0.74})
     @currency_code_hash = currency_code_hash
-  end
+  end                                                                 # => :initialize
 
   def currency_code_hash
     @currency_code_hash
-  end
+  end                     # => :currency_code_hash
 
   def convert(a_currency, a_currency_code)
     if a_currency.currency_code == a_currency_code
       Currency.new(a_currency.amount, a_currency_code)
     elsif (currency_code_hash.has_key? a_currency.currency_code) &&
           (currency_code_hash.has_key? a_currency_code)
-      new_currency_amount = a_currency.amount * (currency_code_hash[a_currency.currency_code] / currency_code_hash[a_currency_code])
+      new_currency_amount = a_currency.amount * (currency_code_hash[a_currency_code] / currency_code_hash[a_currency.currency_code] )
       Currency.new(new_currency_amount, a_currency_code)
     else
       raise UnknownCurrencyCodeError
     end
-  end
+  end                                                                                                                                  # => :convert
 
-end
+end  # => :convert
